968384d7dc320a01dfa60bf697dca474d73df1e4
Elman <holdingscythe@zoznam.sk>
2017-04-27 12:23:45 +0200

    Added custom font to settings

--- PocketAMCReader/src/main/java/com/holdingscythe/pocketamcreader/settings/SettingsActivity.java
+++ PocketAMCReader/src/main/java/com/holdingscythe/pocketamcreader/settings/SettingsActivity.java
@@ -1,13 +1,27 @@
 package com.holdingscythe.pocketamcreader.settings;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+
+import com.holdingscythe.pocketamcreader.R;
+import com.holdingscythe.pocketamcreader.S;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set default font
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(S.DEFAULT_FONT)
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
@@ -18,5 +32,10 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
